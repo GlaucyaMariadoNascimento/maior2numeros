76db5f2e7fd459fad559e46bd2fb8ebdc3cc27d6
Savioli Pri <stobbeglaucya@gmail.com>
2021-10-27 12:02:30 -0300

    Resolução exercicio maior2numeros

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace maior2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        int numero=0, maior=0 ;
+
+        Console.WriteLine("Determine o maior valor");
+
+        Console.Write("Informe o 1 valor: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        maior = numero;
+
+        Console.Write("Informe o 2 valor: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+        if (numero > maior){
+            maior = numero;
+        }
+        Console.WriteLine("O maior valor informado é: "+maior);
+        Console.ReadKey();
+        }
+    }
+}
